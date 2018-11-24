16d268c14892429ec926e5f93177d88d5360802b
lwileczek <30635219+lwileczek@users.noreply.github.com>
2018-11-24 08:51:31 -0500

    Update my_sort.py

--- my_sort.py
+++ my_sort.py
@@ -4,7 +4,7 @@
 Sorting algorithms to write:
 
   - [X]  Bucket sort
-  - [ ]  Bubble sort
+  - [X]  Bubble sort
   - [ ]  Insertion sort
   - [ ]  Selection sort
   - [ ]  Heapsort
@@ -56,7 +56,31 @@ def bucket_sort(x, max_val=0):
                 idx += 1
 
     return sorted_list
-    
+
+
+def bubble_sort(x):
+    """
+    Bubble sort algorithm: Go through list and if the number after is larger
+    than the number before switch them. Continue this until you go through the
+    list and it makes so switches.
+    """
+    list_copy = x.copy()
+    while True:
+        flag = 1
+        for idx, first, second in zip(range(len(x)), x[:-1], x[1:]):
+            if first > second:
+                tmp = list_copy[idx]
+                list_copy[idx] = list_copy[idx + 1]
+                list_copy[idx + 1] = tmp
+                if flag:
+                    flag = 0
+
+        if flag:  # if there was no switch after all checks then exit
+            break
+        x = list_copy.copy()  # else update x from copy and run again
+
+    return x
+
 
 def srt(x, sort_type='quick', max_val=None):
     """
@@ -65,8 +89,11 @@ def srt(x, sort_type='quick', max_val=None):
     assert isinstance(x, list), "Input must be a list"
     if len(x) < 2:
         return x
-    if sort_type == 'bucket':
+    if sort_type.lower() == 'bucket':
         sorted_list = bucket_sort(x)
+    elif sort_type.lower() == 'bubble':
+        sorted_list = bubble_sort(x)
+
     return sorted_list
     
 
@@ -74,15 +101,33 @@ if __name__ == "__main__":
     import numpy as np
     PRNG = np.random.RandomState()
     def test_sort():
+## Create lists to test on
         tst0 = list(PRNG.randint(0, 15, size=6)) 
         tst1 = list(PRNG.randint(0, 15, size=6)) 
+        tst2 = list(PRNG.randint(0, 115, size=70)) 
 
-        assert  srt(tst1, sort_type = 'bucket') == sorted(tst1), 'Test case {}'\
-        ' failed.'.format(tst1)
+## Test Null and single case
         assert srt([], sort_type = 'bucket') == [], 'error with empty case'
         assert srt([0], sort_type = 'bucket') == [0], 'error with [0]'
         assert srt([1], sort_type = 'bucket') == [1], 'error with [1]'
-        ans0 = srt(tst0, sort_type = 'bucket')
-        print("Fist list:", tst0, "\n""outcome:", ans0)
 
-    test_sort()
+## Test Bucket Sort
+        assert  srt(tst1, sort_type = 'bucket') == sorted(tst1), 'Test case {}'\
+        ' failed.'.format(tst1)
+        assert  srt(tst2, sort_type = 'bucket') == sorted(tst2), 'Test case {}'\
+        ' failed.'.format(tst2)
+        ans0 = srt(tst0, sort_type = 'bucket')
+        print("Bucket Sort:\n\tInput:", tst0, "\n\tOutcome:", ans0, "\n")
+
+## Test Bubble Sort
+        assert  srt(tst1, sort_type = 'bubble') == sorted(tst1), 'Test case {}'\
+        ' failed.'.format(tst1)
+        assert  srt(tst2, sort_type = 'bubble') == sorted(tst2), 'Test case {}'\
+        ' failed.'.format(tst2)
+        assert  srt(tst0, sort_type = 'bubble') == sorted(tst0), 'Test case {}'\
+        ' failed.'.format(tst0)
+        print("Bubble Sort:\n\tInput:", tst0, "\n\tOutcome:", 
+            srt(tst0, sort_type='bubble'),"\n")
+
+    test_sort()  # run test function 
+    
